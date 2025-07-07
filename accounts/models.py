1a93be844ed3fa5b4e99dd56f1a1ec552621bea5
Andriy <grushevoyandrey@gmail.com>
2025-07-07 19:38:40 +0100

    Add custom User model and UserManager with email authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email is required.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields["is_staff"]:
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields["is_superuser"]:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    ROLE_CHOICES = (
+        ('admin', "Admin"),
+        ('user', "User"),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    objects = UserManager()
+
 
 # Create your models here.
