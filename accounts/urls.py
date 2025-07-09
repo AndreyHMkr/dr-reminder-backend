69e9ccdc29f4aa5ee7d58e778ffdfa604ad20cf9
Andriy <grushevoyandrey@gmail.com>
2025-07-09 20:11:27 +0100

    Add URL routes for registration, JWT auth, and password reset views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-
-from accounts.views import CreateUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from accounts.views import CreateUserView, ResetPasswordView, ResetPasswordConfirmView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
+    path("reset-password-confirm/", ResetPasswordConfirmView.as_view(), name="reset-password-confirm"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify")
 ]
