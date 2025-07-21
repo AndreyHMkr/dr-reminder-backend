5b421d8fa4d5feeae68c1142f426614480705b84
Andriy <grushevoyandrey@gmail.com>
2025-07-21 20:43:40 +0100

    add 'Speciality' model

--- services/models.py
+++ services/models.py
@@ -15,3 +15,19 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Speciality(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name="specialities")
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order"]
+        verbose_name = "Speciality"
+        verbose_name_plural = "Specialities"
+
+    def __str__(self):
+        return self.name
