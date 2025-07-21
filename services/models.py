52d89b10cc3d0291e851b0872ff9c73e20f21a82
Andriy <grushevoyandrey@gmail.com>
2025-07-21 20:30:37 +0100

    Add Service model

--- services/models.py
+++ services/models.py
@@ -1,1 +1,17 @@
+from django.db import models
 
+
+class Service(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    icon = models.CharField(max_length=100, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order"]
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return self.title
