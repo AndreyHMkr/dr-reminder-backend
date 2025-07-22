ce00ed0fd438944938875c109b91203e0ab9a9c8
Andriy <grushevoyandrey@gmail.com>
2025-07-22 20:42:09 +0100

    Add SpecialityViewSet to expose medical specialities via API

--- services/views.py
+++ services/views.py
@@ -1,11 +1,16 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from services.models import Service
-from services.serializers import ServiceSerializer
+from services.models import Service, Speciality
+from services.serializers import ServiceSerializer, SpecialitySerializer
 
 
 class ServiceViewSet(ReadOnlyModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [AllowAny]
+
+class SpecialityViewSet(ReadOnlyModelViewSet):
+    queryset = Speciality.objects.all()
+    serializer_class = SpecialitySerializer
     permission_classes = [AllowAny]
