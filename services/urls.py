12c9407445d1d62a4a0e6f201efeb2ec9a9b88ab
Andriy <grushevoyandrey@gmail.com>
2025-07-22 20:42:52 +0100

    Register SpecialityViewSet route for medical specialities API

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from services.views import ServiceViewSet
+from services.views import ServiceViewSet, SpecialityViewSet
 
 router = DefaultRouter()
-router.register('services', ServiceViewSet, basename='services')
+router.register("services", ServiceViewSet, basename="services")
+router.register("speciality", SpecialityViewSet, basename="speciality")
 urlpatterns = router.urls
