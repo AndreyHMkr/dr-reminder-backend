fbf34998f184d4ad78404d0e06219d89875a98ac
Andriy <grushevoyandrey@gmail.com>
2025-07-23 19:18:02 +0100

    Replaced database from sqlite to postgres

--- dr_reminder_api/settings.py
+++ dr_reminder_api/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'dr_reminder_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
@@ -142,9 +146,7 @@ SITE_NAME = os.getenv("SITE_NAME", "Your App")
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 INTERNAL_IPS = [
-    # ...
     "127.0.0.1",
-    # ...
 ]
 
 CORS_ALLOWED_ORIGINS = [
