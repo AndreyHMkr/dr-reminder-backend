ecdb6302154e5298de3f83f59d2711fd93e20f0e
Andriy <grushevoyandrey@gmail.com>
2025-07-24 20:39:54 +0100

    add login/logout path

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView,
+    TokenVerifyView, TokenBlacklistView,
 )
-from accounts.views import CreateUserView, ResetPasswordView, ResetPasswordConfirmView, UserProfileView
+from accounts.views import CreateUserView, ResetPasswordView, ResetPasswordConfirmView, UserProfileView, LogoutView
 
 urlpatterns = [
                   path("register/", CreateUserView.as_view(), name="register"),
+                  path("login/", TokenObtainPairView.as_view(), name="login"),
+                  path("logout/", LogoutView.as_view(), name="logout"),
                   path("profile/", UserProfileView.as_view(), name="profile"),
                   path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
                   path("reset-password-confirm/", ResetPasswordConfirmView.as_view(), name="reset-password-confirm"),
