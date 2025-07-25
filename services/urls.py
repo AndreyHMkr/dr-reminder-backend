ecf13223422451794dbdc7898adfc7fd04343492
Andriy <grushevoyandrey@gmail.com>
2025-07-25 21:24:33 +0100

    add 'medical-specialties' path

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from services.views import ServiceViewSet
+from services.views import ServiceViewSet, MedicalSpecialtyViewSet
 
 router = DefaultRouter()
 router.register('services', ServiceViewSet, basename='services')
+router.register("medical-specialties", MedicalSpecialtyViewSet, basename='medical-specialty')
 urlpatterns = router.urls
