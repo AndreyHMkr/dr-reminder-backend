1e87457533ee23222d21b061823cc44849dd26ed
Andriy <grushevoyandrey@gmail.com>
2025-07-25 21:20:33 +0100

    add models and viewsets for service-related functionality

--- services/models.py
+++ services/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from dr_reminder_api import settings
+
 
 class Service(models.Model):
     title = models.CharField(max_length=100, unique=True)
@@ -15,3 +17,28 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+
+class MedicalSpecialty(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+
+class Event(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Vaccination(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    vaccine_info = models.TextField()
+
+class AnalysisPackage(models.Model):
+    title = models.CharField(max_length=100)
+
+class AnalysisTest(models.Model):
+    package = models.ForeignKey(AnalysisPackage, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
