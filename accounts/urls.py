c880356624a6595c782caab4c73aee755a7925c6
Andriy <grushevoyandrey@gmail.com>
2025-07-26 14:22:07 +0100

    deleted the auxiliary path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView, TokenBlacklistView,
+    TokenVerifyView,
 )
 from accounts.views import CreateUserView, ResetPasswordView, ResetPasswordConfirmView, UserProfileView, LogoutView
 
@@ -14,7 +13,6 @@ urlpatterns = [
                   path("profile/", UserProfileView.as_view(), name="profile"),
                   path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
                   path("reset-password-confirm/", ResetPasswordConfirmView.as_view(), name="reset-password-confirm"),
-                  path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
                   path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
                   path("token/verify/", TokenVerifyView.as_view(), name="token_verify")
-              ] + debug_toolbar_urls()
+              ]
