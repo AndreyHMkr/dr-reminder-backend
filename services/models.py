30fe6f28dd628f442d380f24bda874b91cbc1225
Andriy <grushevoyandrey@gmail.com>
2025-07-27 12:04:59 +0100

    Refactor Event model: split into specific event types with related fields

--- services/models.py
+++ services/models.py
@@ -27,7 +27,23 @@ class Event(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date = models.DateField(null=True, blank=True)
+    medical_specialty = models.ForeignKey("MedicalSpecialty", null=True, blank=True, on_delete=models.SET_NULL)
+    vaccination = models.ForeignKey("Vaccination", null=True, blank=True, on_delete=models.SET_NULL)
+    analysis_test = models.ForeignKey("AnalysisTest", null=True, blank=True, on_delete=models.SET_NULL)
+    blood_donation = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.medical_specialty:
+            self.name = f"Visit to {self.medical_specialty.title}"
+        elif self.vaccination:
+            self.name = f"Vaccination against {self.vaccination.title}"
+        elif self.analysis_test:
+            self.name = self.analysis_test.title
+        elif self.blood_donation:
+            self.name = "Blood donation"
+        super().save(*args, **kwargs)
+
 
 class Vaccination(models.Model):
     title = models.CharField(max_length=100, unique=True)
