b9ca17309d7d398560889dbf372e8655afc7296c
Andriy <grushevoyandrey@gmail.com>
2025-07-27 13:57:41 +0100

    add str method for 'AnalysisTest' class

--- services/models.py
+++ services/models.py
@@ -71,3 +71,6 @@ class AnalysisTest(models.Model):
     package = models.ForeignKey(AnalysisPackage, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
