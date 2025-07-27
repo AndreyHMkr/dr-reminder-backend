1bab828138b9e4cfe6059c742485d2d51f007157
Andriy <grushevoyandrey@gmail.com>
2025-07-27 12:06:29 +0100

    deleted 'DEFAULT_PERMISSION_CLASSES'

--- dr_reminder_api/settings.py
+++ dr_reminder_api/settings.py
@@ -128,9 +128,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+
 }
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
