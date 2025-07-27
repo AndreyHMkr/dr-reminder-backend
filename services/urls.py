1b8079ad4da05d1a591481e7409ed1ab4de30260
Andriy <grushevoyandrey@gmail.com>
2025-07-27 12:07:12 +0100

    Register additional viewsets in services router

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
-from services.views import ServiceViewSet, MedicalSpecialtyViewSet
+from services.views import ServiceViewSet, MedicalSpecialtyViewSet, EventViewSet, VaccinationViewSet, \
+    AnalysisPackageViewSet, AnalysisTestViewSet
 
 router = DefaultRouter()
 router.register('services', ServiceViewSet, basename='services')
 router.register("medical-specialties", MedicalSpecialtyViewSet, basename='medical-specialty')
+router.register("events", EventViewSet, basename="events")
+router.register("vaccinations", VaccinationViewSet, basename="vaccinations")
+router.register("analysis-packages", AnalysisPackageViewSet, basename="analysis-packages")
+router.register("analysis-tests", AnalysisTestViewSet, basename="analysis-tests")
 urlpatterns = router.urls
