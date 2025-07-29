0ca977f961361eec10f170f249a86e15bd7e9279
Andriy <grushevoyandrey@gmail.com>
2025-07-29 12:08:10 +0100

    temporary branch

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.http import JsonResponse
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -21,7 +22,6 @@ urlpatterns = [
                   path("profile/", UserProfileView.as_view(), name="profile"),
                   path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
                   path("reset-password-confirm/", ResetPasswordConfirmView.as_view(), name="reset-password-confirm"),
-                  path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-                  path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
                   path("token/verify/", TokenVerifyView.as_view(), name="token_verify")
               ] + debug_toolbar_urls()
+
