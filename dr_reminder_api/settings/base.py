fbfb94fed88265eef32af53750ec56ddcf059a1f
Andriy <grushevoyandrey@gmail.com>
2025-07-30 11:37:05 +0100

    temporary branch

--- dr_reminder_api/settings/base.py
+++ dr_reminder_api/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
