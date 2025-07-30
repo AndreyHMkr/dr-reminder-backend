37d494d8edb0885ba44e80bbd143ef10946927c5
Andriy <grushevoyandrey@gmail.com>
2025-07-30 11:13:38 +0100

    temporary branch

--- dr_reminder_api/settings/base.py
+++ dr_reminder_api/settings/base.py
@@ -125,6 +125,6 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://vovanchu.github.io/Welltrack"
+    "https://vovanchu.github.io"
 ]
 
