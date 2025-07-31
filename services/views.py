65673abc5f91f734abfd88e7105ccf7c8a9f5b8f
Andriy <grushevoyandrey@gmail.com>
2025-07-31 12:12:13 +0100

    add get_queryset

--- services/views.py
+++ services/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from services.models import Service, MedicalSpecialty, Event, Vaccination, AnalysisPackage, AnalysisTest
@@ -20,7 +19,10 @@ class MedicalSpecialtyViewSet(ReadOnlyModelViewSet):
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
