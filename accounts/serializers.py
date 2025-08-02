5fa9122b045471a155ba0d9f748f61f9b1193ebf
Andriy <grushevoyandrey@gmail.com>
2025-08-02 19:16:25 +0100

    Updated UserProfileSerializer & add image_profile field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -81,7 +81,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    username = serializers.CharField()
+    username = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     email = serializers.EmailField(read_only=True)
     class Meta:
         model = UserProfile
@@ -94,6 +94,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "sex",
             "country",
             "city",
+            "image_profile"
         )
     extra_kwargs = {
         "user": {"write_only": True},
