a459d2bc92b646bd77ba61302e5e30da64d1c9ba
Andriy <grushevoyandrey@gmail.com>
2025-08-03 15:12:31 +0100

    add treatment-plans path

--- services/urls.py
+++ services/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
 from services.views import ServiceViewSet, MedicalSpecialtyViewSet, EventViewSet, VaccinationViewSet, \
-    AnalysisPackageViewSet, AnalysisTestViewSet
+    AnalysisPackageViewSet, AnalysisTestViewSet, TreatmentPlanViewSet
 
 router = DefaultRouter()
 router.register('services', ServiceViewSet, basename='services')
 router.register("medical-specialties", MedicalSpecialtyViewSet, basename='medical-specialty')
 router.register("events", EventViewSet, basename="events")
+router.register("treatment-plans", TreatmentPlanViewSet, basename="treatment-plans")
 router.register("vaccinations", VaccinationViewSet, basename="vaccinations")
 router.register("analysis-packages", AnalysisPackageViewSet, basename="analysis-packages")
 router.register("analysis-tests", AnalysisTestViewSet, basename="analysis-tests")
