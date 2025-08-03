964fbfb599a0104c8b4d5bb997d488986882f4b5
Andriy <grushevoyandrey@gmail.com>
2025-08-03 21:32:36 +0100

    add TelegramAccount class

--- telegram_notifications/models.py
+++ telegram_notifications/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class TelegramAccount(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="telegram"
+    )
+    chat_id = models.CharField(max_length=32, unique=True)
+    is_active = models.BooleanField(default=True)
+    linked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_id}:{self.chat_id}"
