7e8456f6564e8cbcd77bf3b68df3147a936de3dd
Andriy <grushevoyandrey@gmail.com>
2025-08-10 12:14:16 +0100

    test

--- dr_reminder_api/celery.py
+++ dr_reminder_api/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dr_reminder_api.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dr_reminder_api.settings.prod")
 
 app = Celery("dr_reminder_api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
